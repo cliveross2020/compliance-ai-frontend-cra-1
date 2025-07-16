0cd493ae34339d106066f2a62769b2e2488d22bb
cliveross2020 <cliveross2020@gmail.com>
2025-07-17 00:34:02 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import Chatbot from './components/Chatbot';  // Assuming you have a chatbot component
+
+function App() {
+  return (
+    <div>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
