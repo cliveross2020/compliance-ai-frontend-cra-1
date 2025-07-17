6da91a24605d2b78c952711d4b836b3bca51f35f
cliveross2020 <cliveross2020@gmail.com>
2025-07-17 12:07:39 +0100

    Delete src directory

--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <section className="Hero">
-      <h1>Welcome to Compliance AI</h1>
-      <p>Your AI-powered assistant for pharmaceutical compliance.</p>
-    </section>
-  );
-}
-export default Home;
