8e234911f6997f20061cda804c54907a0f8ade3f
cliveross2020 <cliveross2020@gmail.com>
2025-07-17 13:34:54 +0100

    Add files via upload

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="DashboardPreview">
+      <h2>Dashboard</h2>
+      <p>This is the dashboard view.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
