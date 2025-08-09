efe2db467ea631e35a4a39c839965b6abe89dd7b
cliveross2020 <cliveross2020@gmail.com>
2025-08-09 08:53:45 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './styles.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import AbpiSearch from './pages/AbpiSearch';
+import GlobalComparator from './pages/GlobalComparator';
+import PmcaseSearch from './pages/PmcaseSearch';
+import Dashboard from './pages/Dashboard';
+import News from './pages/News';
+import AskCAI from './pages/AskCAI';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      <Navbar />
+
+      <main className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/askcai" element={<AskCAI />} />
+          <Route path="/abpi" element={<AbpiSearch />} />
+          <Route path="/comparator" element={<GlobalComparator />} />
+          <Route path="/cases" element={<PmcaseSearch />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/news" element={<News />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
